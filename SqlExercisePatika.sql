d44da8f5a2fd3c3b075ee1a0b1ea829400e73ae9
erenhuseyin <37180505+erenhuseyin@users.noreply.github.com>
2022-12-22 20:43:04 +0300

    Update SqlExercisePatika.sql
    sql+

--- SqlExercisePatika.sql
+++ SqlExercisePatika.sql
@@ -57,20 +57,24 @@ SELECT * FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
 
 --ODEV4--
 /*
-1-country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
-2-country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
-3-film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
-4-film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+1-film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
+2-film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
+3-film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+4-country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+5-city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
 */
 
 --1-
-SELECT * FROM country WHERE country LIKE 'A%' AND country LIKE '%a';
+SELECT DISTINCT replacement_cost FROM film
 --2-
-SELECT country FROM country WHERE country LIKE '_____%n';
+SELECT COUNT(DISTINCT replacement_cost) FROM film
 --3-
-SELECT title FROM film WHERE title ILIKE '%t%t%t%t%';
+SELECT COUNT(*) FROM film WHERE title LIKE 'T%'AND rating IN ('G');
 --4-
-SELECT * FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
+SELECT COUNT(country) FROM country WHERE country LIKE '_____'
+--5-
+SELECT COUNT(*) FROM city WHERE  city ILIKE '%r'
 
 
 --ODEV5--
